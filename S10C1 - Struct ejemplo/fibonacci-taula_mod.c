906a5381f28b53be62d3e682aa6ced0cffbff50c
Nahuel Statuto <nahuel.statuto@gmail.com>
2025-04-29 12:55:14 +0200

    s9 y 10

---
+++ S10C1 - Struct ejemplo/fibonacci-taula_mod.c
@@ -0,0 +1,77 @@
+/* fibonacci-taula_mod.c (fitxer modificat a clase, que compila i executa )
+Exemple on es treballa amb:
+   tipus estrucutra que conté un camps que és un vector (de longitud fixa)
+   assignació de varibles estructura
+   (no es poden assignar vectors, però sí estructures)
+---------------------------------------------------------
+
+Es calculen N termes de la successió de Fibonacci
+   x(n) = x(n-1) + x(n-2)
+començant a x(0) = 0, x(1) = 1,
+guardant-los en una variable d'un tipus estructurat especial
+
+Es fan escriure
+
+Després s'eliminen els elements que són múltiples de 7
+
+Es fan escriure els que queden
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 20
+
+typedef long int *tauFib;   
+
+typedef struct {
+   long int *t;
+   int n;
+} taula;               
+
+void escriureFib( taula );   
+void recur( taula* );   /*usando tauFib[20]*/
+/*void recur( taula*, k );    memoria dinamica*/
+
+int main (void) 
+{
+   taula fib;           
+   tauFib a;
+   int i, j, k;
+
+   scanf("%d",&k);
+
+   fib.t = (long int *)malloc(k*sizeof(long int));
+   a = (tauFib)malloc(k*sizeof(tauFib));
+   fib.t[0] = 0;
+   fib.t[1] = 1;
+   fib.n = 2;
+   while (fib.n < k ) {
+      fib.t[fib.n] = fib.t[fib.n-1] + fib.t[fib.n-2];
+      ++(fib.n);
+   }
+   printf( "Nombres de Fibonacci\n" );
+   escriureFib(fib);
+   for (i = 0; i < fib.n; ++i) {
+      if (fib.t[i] % 7 == 0) {
+         for (j = i + 1; j < fib.n; ++j) {
+            fib.t[j-1] = fib.t[j];
+         }
+         --(fib.n);
+      }
+   }   
+   printf( "Nombres de Fibonacci sense multiples de 7\n" );
+   escriureFib(fib);
+   free(fib.t);
+   free(a);
+   return 0;
+}
+void escriureFib(taula f) 
+{  int i;
+   for (i = 0; i < f.n; ++i) {
+      printf( " %ld" , f.t[i]);
+   }
+   printf( "\n" );
+   
+   return;
+}
