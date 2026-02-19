20442098c07a8e8335b4b89e325cb84f8bc33a49
Nahuel Statuto <nahuel.statuto@gmail.com>
2026-02-19 12:36:56 +0100

    cambios nuevo  curso

--- S2C2 - Punteros I/movemos la memoria/z.c
+++ S2C2 - Punteros I/movemos la memoria/z.c
@@ -8,31 +8,30 @@ int main(void){
 
    int v=1, a=2, b=3, c=5, d=7;
 
-   printf("v=%d, \t adress=%p \n", v, &v);
-   printf("a=%d, \t adress=%p \n", a, &a);
-   printf("b=%d, \t adress=%p \n", b, &b);
-   printf("c=%d, \t adress=%p \n", c, &c);
-   printf("d=%d, \t adress=%p \n", d, &d); 
+   printf("v=%d, \t direccio=%p \n", v, &v);
+   printf("a=%d, \t direccio=%p \n", a, &a);
+   printf("b=%d, \t direccio=%p \n", b, &b);
+   printf("c=%d, \t direccio=%p \n", c, &c);
+   printf("d=%d, \t direccio=%p \n", d, &d); 
 
    printf("\n");
 
-   *(&v+1) = 11;
-   *(&v+2) = 13;
-   *(&v+3) = 17;
-   
+   *(&d+1) = 11; /*c*/
+   *(&d+2) = 13; /*b*/
+   *(&d+3) = 17; /*a*/   
 
-   printf("v1=%d, \t adress=%p \n", v, &v);
-   printf("v2=%d, \t adress=%p \n", *(&v+1), &v+1);
-   printf("v3=%d, \t adress=%p \n", *(&v+2), &v+2);
-   printf("v4=%d, \t adress=%p \n", *(&v+3), &v+3);
+   printf("v1=%d, \t direccio=%p \n", d, &d);
+   printf("v2=%d, \t direccio=%p \n", *(&d+1), &d+1);
+   printf("v3=%d, \t direccio=%p \n", *(&d+2), &d+2);
+   printf("v4=%d, \t direccio=%p \n", *(&d+3), &d+3);
    
    printf("\n");
 
 
-   printf("a=%d, \t adress=%p \n", a, &a);
-   printf("b=%d, \t adress=%p \n", b, &b);
-   printf("c=%d, \t adress=%p \n", c, &c);
-   printf("d=%d, \t adress=%p \n", d, &d);
+   printf("a=%d, \t direccio=%p \n", a, &a);
+   printf("b=%d, \t direccio=%p \n", b, &b);
+   printf("c=%d, \t direccio=%p \n", c, &c);
+   printf("d=%d, \t direccio=%p \n", d, &d);
 
    return 0;
 }
