51c16ee565435cb7158f7dd7a872c3e1a75d4079
Nahuel Statuto <nahuel.statuto@gmail.com>
2026-03-03 11:10:49 +0100

    presentacio

--- S4C1 - Más punteros/funciones_return_puntero.c
+++ S4C1 - Más punteros/funciones_return_puntero.c
@@ -3,21 +3,20 @@
 #include <math.h>
 
 
-double **crear_matriz(int); /* Función para asignar memoria a una matriz de tamaño n */
+double** crear_matriz(int); /* Función para asignar memoria a una matriz de tamaño n */
 void liberar_matriz(double **, int);/* Función para liberar memoria de una matriz */
-
 
 /* Programa principal */
 int main() {
     int n;
     double **A;
-
+    printf("MemA_main: %p", &A);
     /* Solicitar el tamaño de la matriz y el orden de la expansión */
     printf("Ingrese la dimensión de la matriz: ");
     scanf("%d", &n);
     
     /* Asignar memoria a las matrices */
-    A = crear_matriz(n);
+    A = crear_matriz(n); /*return direccion de memoria*/
     liberar_matriz(A, n);
     
     return 0;
@@ -25,19 +24,22 @@ int main() {
 
 double **crear_matriz(int n) {
     int i;
-    double **matriz = (double **)malloc(n * sizeof(double *));
-    if (matriz == NULL) {
+    double **A;
+    printf("MemA_funct: %p", &A);
+
+    A = (double **)malloc(n * sizeof(double *));
+    if (A == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
         exit(1);
     }
     for (i = 0; i < n; i++) {
-        matriz[i] = (double *)malloc(n * sizeof(double));
-        if (matriz[i] == NULL) {
+        A[i] = (double *)malloc(n * sizeof(double));
+        if (A[i] == NULL) {
             printf("Error: No se pudo asignar memoria.\n");
             exit(1);
         }
     }
-    return matriz;
+    return A;
 }
 
 void liberar_matriz(double **matriz, int n) {
